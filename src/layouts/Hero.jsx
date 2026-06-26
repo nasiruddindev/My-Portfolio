0811359b5cfb6b061ae90d2ab4e82be7f0767493
Nasir <dev.nasiruddin@gmail.com>
2026-06-26 11:40:41 +0600

    feat:add some new features

--- src/layouts/Hero.jsx
+++ src/layouts/Hero.jsx
@@ -6,52 +6,53 @@ import { FaGithub, FaLinkedinIn, FaTwitter } from 'react-icons/fa6'
 import Banner from '../assets/banner.png'
 import Image from '../components/Image'
 import { Link } from 'react-router-dom'
+import CV from '../assets/nasiruddin.pdf'
 
 const Hero = () => {
   return (
     <section className="bg-slate-400">
       <Container>
-          <div>
-            <h1 className="text-center text-primary font-bold font-inter text-2xl md:text-5xl py-5">
-              Hi I'm
-              <span className="text-blue-700 pl-3 text-3xl md:text-6xl">
-                Nasir Uddin
-              </span>
-            </h1>
+        <div>
+          <h1 className="text-center text-primary font-bold font-inter text-2xl md:text-5xl py-5">
+            Hi I'm
+            <span className="text-blue-700 pl-3 text-3xl md:text-6xl">
+              Nasir Uddin
+            </span>
+          </h1>
 
-            <p className="text-secondary text-xl px-4 md:px-0 text-center  md:text-2xl font-medium font-inter max-w-110 mx-auto">
-              I build modern,responsive and user friendlly web applications with
-              React
-            </p>
-            <div className='py-6 text-center'>
+          <p className="text-secondary text-xl px-4 md:px-0 text-center  md:text-2xl font-medium font-inter max-w-110 mx-auto">
+            I build modern,responsive and user friendlly web applications with
+            React
+          </p>
+          <div className="py-6 text-center">
+            <a href={CV} download="">
               <Button text="Resume" />
-            </div>
+            </a>
+          </div>
 
-            <div className='flex gap-x-3  justify-center pb-10 '>
-              <Link to="https://github.com/nasiruddindev" target='_blank'>
+          <div className="flex gap-x-3  justify-center pb-10 ">
+            <Link to="https://github.com/nasiruddindev" target="_blank">
               <div className="border border-black/50 w-8 md:w-10 h-8 md:h-10  rounded-full flex justify-center items-center cursor-pointer">
-              <FaGithub className='text-xl md:text-2xl '/>
-            </div>
-              </Link>
-
-
-            <Link to="https://www.linkedin.com/in/devnasiruddin/" target='_blank'>
-            <div className="border border-black/50 w-8 md:w-10 h-8 md:h-10  rounded-full flex justify-center items-center cursor-pointer">
-              <FaLinkedinIn  className='text-xl md:text-2xl '/>
-            </div>
+                <FaGithub className="text-xl md:text-2xl " />
+              </div>
             </Link>
 
+            <Link
+              to="https://www.linkedin.com/in/devnasiruddin/"
+              target="_blank"
+            >
+              <div className="border border-black/50 w-8 md:w-10 h-8 md:h-10  rounded-full flex justify-center items-center cursor-pointer">
+                <FaLinkedinIn className="text-xl md:text-2xl " />
+              </div>
+            </Link>
 
             <Link to="https://x.com/nasiruddindev" target="_blank">
-            <div className="border border-black/50 w-8 md:w-10 h-8 md:h-10  rounded-full flex justify-center items-center cursor-pointer">
-              <FaTwitter  className='text-xl md:text-2xl '/>
-            </div>
+              <div className="border border-black/50 w-8 md:w-10 h-8 md:h-10  rounded-full flex justify-center items-center cursor-pointer">
+                <FaTwitter className="text-xl md:text-2xl " />
+              </div>
             </Link>
-
-            </div>
-
-
           </div>
+        </div>
       </Container>
     </section>
   )
